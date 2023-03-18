d2cd49d3cfc78d887051f218489de6242e276505
Miron <rybak.m.yu@gmail.com>
2023-03-18 23:51:11 +0300

    сука
    View для второй активити надо настроить

--- app/src/main/java/com/firstapp/moneymeter/CompoundInterest.kt
+++ app/src/main/java/com/firstapp/moneymeter/CompoundInterest.kt
@@ -3,16 +3,16 @@ package com.firstapp.moneymeter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.PersistableBundle
-import com.firstapp.moneymeter.databinding.ActivityMainBinding
+import com.firstapp.moneymeter.databinding.CompoundinterestBinding
 
 
 class CompoundInterest: AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: CompoundinterestBinding
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        binding = CompoundinterestBinding.inflate(layoutInflater).also { setContentView(it.root) }
     }
 
     override fun onStart() {
