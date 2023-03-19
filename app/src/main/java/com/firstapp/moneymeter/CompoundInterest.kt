82b3b17a16cf8290794f7cc9eaba7f51d5d4c085
Miron <rybak.m.yu@gmail.com>
2023-03-20 01:34:18 +0300

    wtf

--- app/src/main/java/com/firstapp/moneymeter/CompoundInterest.kt
+++ app/src/main/java/com/firstapp/moneymeter/CompoundInterest.kt
@@ -2,17 +2,17 @@ package com.firstapp.moneymeter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.os.PersistableBundle
 import com.firstapp.moneymeter.databinding.CompoundinterestBinding
 
 
 class CompoundInterest: AppCompatActivity() {
 
-    private lateinit var binding: CompoundinterestBinding
+    lateinit var binding: CompoundinterestBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        binding = CompoundinterestBinding.inflate(layoutInflater).also { setContentView(it.root) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = CompoundinterestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onStart() {
--- app/src/main/java/com/firstapp/moneymeter/MainActivity.kt
+++ app/src/main/java/com/firstapp/moneymeter/MainActivity.kt
@@ -7,7 +7,7 @@ import com.firstapp.moneymeter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
