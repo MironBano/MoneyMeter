fbfd9339f4598ef3cad7e0eb11d1dc917a11383c
Miron <rybak.m.yu@gmail.com>
2023-03-27 20:30:15 +0300

    новый вид compound

--- app/src/main/java/com/firstapp/moneymeter/CompoundInterest.kt
+++ app/src/main/java/com/firstapp/moneymeter/CompoundInterest.kt
@@ -4,20 +4,21 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.firstapp.moneymeter.databinding.CompoundinterestBinding
+import com.firstapp.moneymeter.databinding.CompoundinteresttestBinding
 import com.google.android.material.slider.Slider
 
 
 class CompoundInterest: AppCompatActivity() {
 
-    lateinit var binding: CompoundinterestBinding
+    lateinit var binding: CompoundinteresttestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        binding = CompoundinterestBinding.inflate(layoutInflater)
+        binding = CompoundinteresttestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.termSlider.addOnChangeListener { slider, value, fromUser ->
-            binding.textTermNumber.text = value.toInt().toString()
+            binding.textTermNumber.text = value.toInt().toString() + " лет"
         }
 
         fun onStart() {
