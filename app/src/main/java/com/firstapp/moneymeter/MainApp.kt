7a839005fc87868fac1e324afb1cf3fa45488cd3
Miron <rybak.m.yu@gmail.com>
2023-06-18 20:51:27 +0300

    добавил тестовый баннер в Requisities.kt

---
+++ app/src/main/java/com/firstapp/moneymeter/MainApp.kt
@@ -0,0 +1,13 @@
+package com.firstapp.moneymeter
+
+import android.app.Application
+import com.yandex.mobile.ads.common.MobileAds
+
+class MainApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MobileAds.initialize(this){
+
+        }
+    }
+}
--- app/src/main/java/com/firstapp/moneymeter/Requisites.kt
+++ app/src/main/java/com/firstapp/moneymeter/Requisites.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.firstapp.moneymeter.databinding.RequisitesBinding
+import com.yandex.mobile.ads.banner.AdSize
+import com.yandex.mobile.ads.common.AdRequest
 
 class Requisites: AppCompatActivity() {
     lateinit var binding: RequisitesBinding
@@ -11,6 +13,11 @@ class Requisites: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = RequisitesBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
+
+        binding.topBanner.setAdUnitId("demo-banner-yandex")
+        binding.topBanner.setAdSize(AdSize.inlineSize(350,70))
+        val adRequest = AdRequest.Builder().build()
+        binding.topBanner.loadAd(adRequest)
 
         with(binding){
             emailGroup.visibility = View.GONE
